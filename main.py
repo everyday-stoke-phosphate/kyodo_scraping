86ae13800149c61d72f024170e00d5e2be8b45ae
test <test@htrghttesjthehukyjthrdjhe.greafewufgwuag>
2020-03-10 22:03:56 +0900

    スクリプトが走ったとき結果にデータが重複してしまうバグを修正(キャッシュファイルのkey_words_not_include.jsonファイルをクリアするように変更)

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ def clear_cash(cash_path_list: list):
 RSS_setting_path = "RSS_setting.yml"
 path_result = "kyodo_articles/data/results.json"
 include_out_path = "URL_list_keyword_include.txt"
+key_words_not_include = "./kyodo_articles/data/key_words_not_include.json"
 start_url = "starts_urls.txt"
-cash_list = [path_result, start_url]
+cash_list = [path_result, start_url,key_words_not_include]
 
 kyodo_RSS.main(RSS_setting_path)
 convert_json_to_url_list.main()
